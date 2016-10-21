8149028526246dc9212e1ba16a80aa19e5735aed
calvin <yanran200830@163.com>
2016-10-21 20:03:07 +0800

    Update new.py

--- new.py
+++ new.py
@@ -8,21 +8,24 @@ def getPage(navBar):
     soups = list()
     for navItem in navBar:
         pages.append(navItem.get("href"))
-        for page in pages:
-            soups.append(BeautifulSoup(requests.get(page).content, "lxml"))
-    print (len(soups))
+    for page in pages:
+        soups.append(BeautifulSoup(requests.get(page).content, "lxml"))
+
     return soups      
 
 def getNewsLinks(soups):
     newsLinks = list()
+    sections = list()
     for soup in soups:
-        sections = soup.select("#content #section a")
-        for links in sections:
+        sections.append(soup.select("#content #section a"))
+    for section in sections:
+        for links in section:
             link = links.get("href")
-            if link not in newsLinks:
-                newsLinks.append(link)
-                print ("新闻链接 ----------" + link)
+            #if link not in newsLinks:
+            newsLinks.append(link)
+            print ("新闻链接 ----------" + link)
     print ("新闻链接总数------" + str(len(newsLinks)))
+    print (len(sections))
     return newsLinks
     
 def main(url):
@@ -33,6 +36,8 @@ def main(url):
     return getNewsLinks(soups)
 
 links = main("http://www.myzaker.com/")
+len(set(links))
+
 
 
 
